678fc3a3f5940ace9e50cf58997f87f3a840383f
Alessio Macrì <m.alessio02@gmail.com>
2025-05-21 12:59:06 +0200

    Upload Prova ereditarieta

---
+++ ProvaEred/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Cane c = new Cane();
+        c.FaiVerso();
+        c.Scodinzola();
+        
+    }
+}
+
+public class Animale
+{
+    public virtual void FaiVerso()
+    {
+        Console.WriteLine($"L'animale fa un verso.");
+    }
+}
+
+public class Cane : Animale
+{
+    public void Scodinzola()
+    {
+        Console.WriteLine($"Il cane scodinzola.");
+    }
+
+    public override void FaiVerso()
+    {
+        base.FaiVerso();
+        Console.WriteLine($"Bau!");
+    }
+}
