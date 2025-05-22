3f8fa3be75a3a2d30dc020aa7d4fbfc18fd3549b
Alessio Macrì <m.alessio02@gmail.com>
2025-05-22 15:44:46 +0200

    upload esercizi sull'incapsulamento 22 maggio

---
+++ esercizi22maggio/EsercizioIncapsulamento1/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Runtime.ConstrainedExecution;
+using Microsoft.VisualBasic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        PrenotazioneViaggio p = new PrenotazioneViaggio();
+
+        Console.Write($"Inserisci la destinazione del viaggio: ");
+        p.Destinazione = Console.ReadLine();
+
+        Console.Write($"Quanti posti vuoi prenotare: ");
+        int pp = int.Parse(Console.ReadLine());
+        p.PrenotaPosti(pp);
+        p.Stampa();
+
+        Console.Write($"Quanti posti vuoi prenotare: ");
+        int pp2 = int.Parse(Console.ReadLine());
+        p.PrenotaPosti(pp2);
+        p.Stampa();
+
+        Console.Write($"Quanti posti vuoi annullare: ");
+        int ap = int.Parse(Console.ReadLine());
+    }
+}
+
+
+
+class PrenotazioneViaggio
+{
+
+    private int _maxPosti = 20;
+    private int _postiPrenotati;
+
+    private string _destinazione;
+
+    private int postiDisponibili;
+
+    public string Destinazione { get; set; }
+
+    //sola lettura
+    public int PostiPrenotati
+    {
+        get { return _postiPrenotati;}
+    }
+
+    //sola lettura, calcolo dei posti non prenotati
+    public int PostiDisponibili
+    {
+        get { return _maxPosti - PostiPrenotati; }
+    }
+
+    //metodo per la prenotazione dei posti
+    public void PrenotaPosti(int n)
+    {
+        if (n == 0)
+        {
+            Console.WriteLine($"Numero non valido");
+        }
+        else if (n <= PostiDisponibili)
+        {
+            _postiPrenotati += n;
+            Console.WriteLine($"La prenotazione è andata a buon fine. Posti prenotati: {n}");
+        }
+        else
+        {
+            Console.WriteLine($"Insufficenti posti disponibili");
+        }
+    }
+
+    //metodo per l'annullazione dei posti prenotati, inverso a quello della prenotazione
+    public void AnnullaPrenotazione(int n)
+    {
+        if (n == 0)
+        {
+            Console.WriteLine($"Numero non valido");
+        }
+        else if (n> 0 && n <= PostiDisponibili)
+        {
+            _postiPrenotati -= n;
+            Console.WriteLine($"L'annullamento è andato a buon fine. Posti annullati: {n}");
+        }
+        else
+        {
+            Console.WriteLine($"Insufficenti posti disponibili");
+        }
+    }
+
+    //tostring per stampare destinazione e posti prenotati e disponibili
+    public void Stampa()
+    {
+        Console.WriteLine($"Destinazione: {Destinazione} \n Posti prenotati: {PostiPrenotati} \n Posti disponibili: {PostiDisponibili}");
+    }
+
+}
---
+++ esercizi22maggio/EsercizioIncapsulamento2/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        VoloAereo v = new VoloAereo();
+
+        Console.WriteLine($"Inserisci il tuo codice di volo: ");
+        v.CodiceVolo = Console.ReadLine();
+        v.VisualizzaStato();
+
+        bool x = true;
+        do
+        {
+            Console.WriteLine($"1: prenota posti");
+            Console.WriteLine($"2: annulla prenotazione");
+            Console.WriteLine($"2: esci");
+            
+            Console.Write($"Cosa vuoi fare? ");
+            int scelta = int.Parse(Console.ReadLine());
+
+            switch (scelta)
+            {
+                case 1:
+                    Console.Write($"Quanti posti vuoi prenotare: ");
+                    int pp = int.Parse(Console.ReadLine());
+                    v.EffettuaPrenotazione(pp);
+                    v.VisualizzaStato();
+                    break;
+                case 2:
+                    Console.Write($"Quanti posti vuoi annullare: ");
+                    int ap = int.Parse(Console.ReadLine());
+                    v.AnnullaPrenotazione(ap);
+                    v.VisualizzaStato();
+                    break;
+                case 3:
+                    x = false;
+                    break;
+                default:
+                    Console.WriteLine($"Scelta non valida");
+                    break;
+            }
+
+        }
+        while (x);
+    }
+
+}
+
+public class VoloAereo
+{
+    private int _postiOccupati = 0;
+    private const int _maxPosti = 150;
+
+    private string _codiceVolo;
+
+    public string CodiceVolo { get; set; }
+    
+
+    public int PostiOccupati
+    {
+        get { return _postiOccupati;}
+    }
+
+    public int PostiLiberi
+    {
+        get { return _maxPosti - _postiOccupati; }
+    }
+
+    public void EffettuaPrenotazione(int n)
+    {
+        if (n == 0)
+        {
+            Console.WriteLine($"Numero non valido");
+        }
+        else if (n <= PostiLiberi)
+        {
+            _postiOccupati += n;
+            Console.WriteLine($"La prenotazione è andata a buon fine. Posti prenotati: {n}");
+        }
+        else
+        {
+            Console.WriteLine($"Insufficenti posti disponibili");
+        }
+    }
+
+    public void AnnullaPrenotazione(int n)
+    {
+        if (n == 0)
+        {
+            Console.WriteLine($"Numero non valido");
+        }
+        else if (n > 0 && n <= PostiLiberi)
+        {
+            _postiOccupati -= n;
+            Console.WriteLine($"L'annullamento è andato a buon fine. Posti annullati: {n}");
+        }
+        else
+        {
+            Console.WriteLine($"Insufficenti posti disponibili");
+        }
+    }
+
+    public void VisualizzaStato()
+    {
+        Console.WriteLine($"Codice volo: {CodiceVolo} ,posti occupati {PostiOccupati}, posti liberi {PostiLiberi}");
+        
+    }
+}
